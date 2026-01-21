d6d3170476c78c810b2033040e0e4871d2ba9df6
Maxim Rychkov <rychkov03@yandex.ru>
2026-01-21 13:11:29 +0500

    feat: add shh and kubeconfig

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -3,10 +3,17 @@
 REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
+CLOUD_IP=$CLOUD_IP
+CLOUD_USER=$CLOUD_USER
+CLUSTER_PORT=$CLUSTER_PORT
 
 cd /home/runner/actions-runner || exit
 
 # sudo service docker start
+
+mkdir -p ~/.ssh/
+ssh-keyscan -H ${CLOUD_IP}  >> ~/.ssh/known_hosts
+autossh -fnNT -L ${CLUSTER_PORT}:localhost:${CLUSTER_PORT} ubuntu@${CLOUD_IP} -i ~/ssh-key
 
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
 
@@ -19,3 +26,4 @@ trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 ./run.sh & wait $!
+
