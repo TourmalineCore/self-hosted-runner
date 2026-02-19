61e24cd711c73ee123eb69088d7e4c505236d250
Maxim Rychkov <rychkov03@yandex.ru>
2026-02-19 08:54:36 +0500

    feat: add expect to automatically pass ssh passphrase

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -3,17 +3,28 @@
 REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
-CLOUD_IP=$CLOUD_IP
-CLOUD_USER=$CLOUD_USER
-CLUSTER_PORT=$CLUSTER_PORT
-
+INNER_CIRCLE_CLOUD_VM_IP=$INNER_CIRCLE_CLOUD_VM_IP
+INNER_CIRCLE_CLOUD_VM_USER=$INNER_CIRCLE_CLOUD_USER
+INNER_CIRCLE_CLOUD_CLUSTER_PORT=$INNER_CIRCLE_CLOUD_CLUSTER_PORT
+INNER_CIRCLE_CLOUD_VM_PASSWORD=$INNER_CIRCLE_CLOUD_VM_PASSWORD
+INNER_CIRCLE_CLOUD_PATH_TO_SSH=$INNER_CIRCLE_CLOUD_PATH_TO_SSH
 cd /home/runner/actions-runner || exit
 
 # sudo service docker start
 
 mkdir -p ~/.ssh/
 ssh-keyscan -H ${CLOUD_IP}  >> ~/.ssh/known_hosts
-autossh -fnNT -L ${CLUSTER_PORT}:localhost:${CLUSTER_PORT} ubuntu@${CLOUD_IP} -i ~/ssh-key
+
+eval `ssh-agent -s`
+expect << EOF
+spawn ssh-add $INNER_CIRCLE_CLOUD_PATH_TO_SSH
+expect "Enter passphrase for $INNER_CIRCLE_CLOUD_PATH_TO_SSH:"
+send "$INNER_CIRCLE_CLOUD_VM_PASSWORD\n";
+expect "Identity added: $INNER_CIRCLE_CLOUD_PATH_TO_SSH *"
+interact
+EOF
+
+autossh -fnNT -L ${INNER_CIRCLE_CLOUD_CLUSTER_PORT}:localhost:${INNER_CIRCLE_CLOUD_CLUSTER_PORT} ubuntu@${INNER_CIRCLE_CLOUD_VM_IP} -i ~/ssh-key
 
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
 
