17f35c9294e2b3e0125ffdad3ff9bdc6aa4ad99a
Maxim Rychkov <rychkov03@yandex.ru>
2026-08-14 11:27:58 +0500

    fix: change ssh var name

--- DockerImage/start.sh
+++ DockerImage/start.sh
@@ -23,7 +23,7 @@ expect "Identity added: /home/runner/$INNER_CIRCLE_CLOUD_PATH_TO_SSH *"
 interact
 EOF
 
-autossh -fnNT -L ${INNER_CIRCLE_CLOUD_CLUSTER_PORT}:localhost:${INNER_CIRCLE_CLOUD_CLUSTER_PORT} ubuntu@${INNER_CIRCLE_CLOUD_VM_IP} -i /home/runner/${INNER_CIRCLE_CLOUD_PATH_TO_>
+autossh -fnNT -L ${INNER_CIRCLE_CLOUD_CLUSTER_PORT}:localhost:${INNER_CIRCLE_CLOUD_CLUSTER_PORT} ubuntu@${INNER_CIRCLE_CLOUD_VM_IP} -i /home/runner/${INNER_CIRCLE_CLOUD_PATH_TO_SSH}
 
 ./config.sh --url https://github.com/${REPOSITORY_OWNER} --token ${REG_TOKEN}
 
